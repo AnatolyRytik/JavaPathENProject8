9363cf2995bdf1b3c631f2988b2e77a0d581308e
Anatoly <anatolyrytik@gmai.com>
2023-05-11 23:26:40 +0200

    add log and javadoc

--- TourGuide/src/main/java/tourGuide/service/RewardsService.java
+++ TourGuide/src/main/java/tourGuide/service/RewardsService.java
@@ -1,89 +1,141 @@
 package tourGuide.service;
 
+import gpsUtil.GpsUtil;
+import gpsUtil.location.Attraction;
+import gpsUtil.location.Location;
+import gpsUtil.location.VisitedLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import rewardCentral.RewardCentral;
+import tourGuide.user.User;
+import tourGuide.user.UserReward;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.springframework.stereotype.Service;
-
-import gpsUtil.GpsUtil;
-import gpsUtil.location.Attraction;
-import gpsUtil.location.Location;
-import gpsUtil.location.VisitedLocation;
-import rewardCentral.RewardCentral;
-import tourGuide.user.User;
-import tourGuide.user.UserReward;
-
+/**
+ * The Rewards service.
+ */
 @Service
 public class RewardsService {
     private static final double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
+    private final GpsUtil gpsUtil;
+    private final RewardCentral rewardsCentral;
+    private final ExecutorService executor;
+    private final Logger log = LoggerFactory.getLogger(RewardsService.class);
 
-	// proximity in miles
+    // proximity in miles
     private int defaultProximityBuffer = 10;
-	private int proximityBuffer = defaultProximityBuffer;
-	private int attractionProximityRange = 200;
-	private final GpsUtil gpsUtil;
-	private final RewardCentral rewardsCentral;
-	private final ExecutorService executor;
+    private int proximityBuffer = defaultProximityBuffer;
+    private int attractionProximityRange = 200;
 
-	public RewardsService(GpsUtil gpsUtil, RewardCentral rewardCentral) {
-		this.gpsUtil = gpsUtil;
-		this.rewardsCentral = rewardCentral;
-		this.executor = Executors.newFixedThreadPool(100);
-	}
+    /**
+     * Instantiates a new Rewards service.
+     *
+     * @param gpsUtil       the gps util
+     * @param rewardCentral the reward central
+     */
+    public RewardsService(GpsUtil gpsUtil, RewardCentral rewardCentral) {
+        this.gpsUtil = gpsUtil;
+        this.rewardsCentral = rewardCentral;
+        this.executor = Executors.newFixedThreadPool(100);
+    }
 
+    /**
+     * Sets proximity buffer.
+     *
+     * @param proximityBuffer the proximity buffer
+     */
+    public void setProximityBuffer(int proximityBuffer) {
+        this.proximityBuffer = proximityBuffer;
+    }
 
-	public void setProximityBuffer(int proximityBuffer) {
-		this.proximityBuffer = proximityBuffer;
-	}
+    /**
+     * Sets default proximity buffer.
+     */
+    public void setDefaultProximityBuffer() {
+        proximityBuffer = defaultProximityBuffer;
+    }
 
-	public void setDefaultProximityBuffer() {
-		proximityBuffer = defaultProximityBuffer;
-	}
+    /**
+     * Calculate rewards for visited locations.
+     *
+     * @param user the user
+     * @return the completable future
+     */
+    public CompletableFuture<Void> calculateRewards(User user) {
+        return CompletableFuture.runAsync(() -> {
+            log.info("Calculating rewards for user: {}", user.getUserName());
 
-	public CompletableFuture<Void> calculateRewards(User user) {
-		return CompletableFuture.runAsync(() -> {
+            List<VisitedLocation> userLocations = new ArrayList<>(user.getVisitedLocations());
+            List<Attraction> attractions = gpsUtil.getAttractions();
+            for (VisitedLocation visitedLocation : userLocations) {
+                for (Attraction attraction : attractions) {
+                    if (user.getUserRewards().stream().noneMatch(r -> r.attraction.attractionName.equals(attraction.attractionName))) {
+                        if (nearAttraction(visitedLocation, attraction)) {
+                            int rewardPoints = getRewardPoints(attraction, user);
+                            user.addUserReward(new UserReward(visitedLocation, attraction, rewardPoints));
+                            log.debug("Added reward for user: {} - Attraction: {} - Points: {}", user.getUserName(), attraction.attractionName, rewardPoints);
+                        }
+                    }
+                }
+            }
+        }, executor);
+    }
 
-			List<VisitedLocation> userLocations = new ArrayList<>(user.getVisitedLocations());
-			List<Attraction> attractions = gpsUtil.getAttractions();
-			for (VisitedLocation visitedLocation : userLocations) {
-				for (Attraction attraction : attractions) {
-					if (user.getUserRewards().stream().noneMatch(r -> r.attraction.attractionName.equals(attraction.attractionName))) {
-						if (nearAttraction(visitedLocation, attraction)) {
-							user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
-						}
-					}
-				}
-			}
-		}, executor);
-	}
+    /**
+     * Check if is within attraction proximity boolean.
+     *
+     * @param attraction the attraction
+     * @param location   the location
+     * @return the boolean
+     */
+    public boolean isWithinAttractionProximity(Attraction attraction, Location location) {
+        log.debug("Checking attraction proximity - Attraction: {} - Location: {}", attraction.attractionName, location);
+        return getDistance(attraction, location) <= attractionProximityRange;
+    }
 
-	public boolean isWithinAttractionProximity(Attraction attraction, Location location) {
-		return getDistance(attraction, location) > attractionProximityRange ? false : true;
-	}
+    private boolean nearAttraction(VisitedLocation visitedLocation, Attraction attraction) {
+        log.debug("Checking if user is near attraction - User: {} - Attraction: {}", visitedLocation.userId, attraction.attractionName);
+        return getDistance(attraction, visitedLocation.location) <= proximityBuffer;
+    }
 
-	private boolean nearAttraction(VisitedLocation visitedLocation, Attraction attraction) {
-		return getDistance(attraction, visitedLocation.location) > proximityBuffer ? false : true;
-	}
+    /**
+     * Gets reward points.
+     *
+     * @param attraction the attraction
+     * @param user       the user
+     * @return the reward points
+     */
+    int getRewardPoints(Attraction attraction, User user) {
+        log.debug("Getting reward points for user: {} - Attraction: {}", user.getUserName(), attraction.attractionName);
+        return rewardsCentral.getAttractionRewardPoints(attraction.attractionId, user.getUserId());
+    }
 
-	int getRewardPoints(Attraction attraction, User user) {
-		return rewardsCentral.getAttractionRewardPoints(attraction.attractionId, user.getUserId());
-	}
-
-	public double getDistance(Location loc1, Location loc2) {
+    /**
+     * Gets distance.
+     *
+     * @param loc1 the loc 1
+     * @param loc2 the loc 2
+     * @return the distance
+     */
+    public double getDistance(Location loc1, Location loc2) {
+        log.debug("Calculating distance - Location 1: {} - Location 2: {}", loc1, loc2);
         double lat1 = Math.toRadians(loc1.latitude);
         double lon1 = Math.toRadians(loc1.longitude);
         double lat2 = Math.toRadians(loc2.latitude);
         double lon2 = Math.toRadians(loc2.longitude);
-
         double angle = Math.acos(Math.sin(lat1) * Math.sin(lat2)
-                               + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+                + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
 
         double nauticalMiles = 60 * Math.toDegrees(angle);
         double statuteMiles = STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
-        return statuteMiles;
-	}
 
+        log.debug("Distance calculated - Location 1: {} - Location 2: {} - Distance: {} statute miles", loc1, loc2, statuteMiles);
+        return statuteMiles;
+    }
 }
--- TourGuide/src/main/java/tourGuide/service/TourGuideService.java
+++ TourGuide/src/main/java/tourGuide/service/TourGuideService.java
@@ -25,22 +25,37 @@ import tourGuide.utils.NearbyAttraction;
 import tripPricer.Provider;
 import tripPricer.TripPricer;
 
+/**
+ * The type Tour guide service.
+ */
 @Service
 public class TourGuideService {
 	private Logger logger = LoggerFactory.getLogger(TourGuideService.class);
 	private final GpsUtil gpsUtil;
 	private final RewardsService rewardsService;
 	private final TripPricer tripPricer = new TripPricer();
+	/**
+	 * The Tracker.
+	 */
 	public final Tracker tracker;
+	/**
+	 * The Test mode.
+	 */
 	boolean testMode = true;
 	private final ExecutorService executor;
 
-	
+
+	/**
+	 * Instantiates a new Tour guide service.
+	 *
+	 * @param gpsUtil        the gps util
+	 * @param rewardsService the rewards service
+	 */
 	public TourGuideService(GpsUtil gpsUtil, RewardsService rewardsService) {
 		this.gpsUtil = gpsUtil;
 		this.rewardsService = rewardsService;
 		this.executor = Executors.newFixedThreadPool(100);
-		
+
 		if(testMode) {
 			logger.info("TestMode enabled");
 			logger.debug("Initializing users");
@@ -50,12 +65,25 @@ public class TourGuideService {
 		tracker = new Tracker(this);
 		addShutDownHook();
 	}
-	
+
+	/**
+	 * Gets user rewards.
+	 *
+	 * @param user the user
+	 * @return the user rewards
+	 */
 	public List<UserReward> getUserRewards(User user) {
+		logger.debug("Getting rewards for user: {}", user.getUserName());
 		return user.getUserRewards();
 	}
 
+	/**
+	 * Gets all users last visited locations.
+	 *
+	 * @return the all users last visited locations
+	 */
 	public Map<UUID, VisitedLocation> getAllUsersLastVisitedLocations() {
+		logger.debug("Getting last visited locations for all users");
 		List<User> allUsers = getAllUsers();
 		Map<UUID, VisitedLocation> lastVisitedLocations = new HashMap<>();
 		for (User user : allUsers) {
@@ -65,7 +93,14 @@ public class TourGuideService {
 		return lastVisitedLocations;
 	}
 
+	/**
+	 * Gets near by attractions.
+	 *
+	 * @param visitedLocation the visited location
+	 * @return the near by attractions
+	 */
 	public List<NearbyAttraction> getNearByAttractions(VisitedLocation visitedLocation) {
+		logger.debug("Getting nearby attractions for visited location of user: {}", visitedLocation.userId);
 		List<Attraction> allAttractions = gpsUtil.getAttractions();
 		List<NearbyAttraction> closestAttractions = new ArrayList<>();
 		User user = getUserById(visitedLocation.userId);
@@ -82,6 +117,12 @@ public class TourGuideService {
 		return closestAttractions.subList(0, Math.min(closestAttractions.size(), 5));
 	}
 
+	/**
+	 * Gets user location.
+	 *
+	 * @param user the user
+	 * @return the user location
+	 */
 	public VisitedLocation getUserLocation(User user) {
 		VisitedLocation visitedLocation;
 		if (user.getVisitedLocations().size() > 0) {
@@ -90,42 +131,84 @@ public class TourGuideService {
 			try {
 				visitedLocation = trackUserLocation(user).get();
 			} catch (InterruptedException | ExecutionException e) {
+				logger.error("Error getting user location", e);
 				throw new RuntimeException("Error getting user location", e);
 			}
 		}
+		logger.debug("Getting location for user: {}", user.getUserName());
 		return visitedLocation;
 	}
-	
+
+	/**
+	 * Gets user.
+	 *
+	 * @param userName the user name
+	 * @return the user
+	 */
 	public User getUser(String userName) {
+		logger.debug("Getting user by username: {}", userName);
 		return internalUserMap.get(userName);
 	}
 
+	/**
+	 * Gets user by id.
+	 *
+	 * @param userId the user id
+	 * @return the user by id
+	 */
 	public User getUserById(UUID userId) {
+		logger.debug("Getting user by ID: {}", userId);
 		return internalUserMap.values().stream()
 				.filter(user -> user.getUserId().equals(userId))
 				.findFirst()
 				.orElse(null);
 	}
 
+	/**
+	 * Gets all users.
+	 *
+	 * @return the all users
+	 */
 	public List<User> getAllUsers() {
+		logger.debug("Getting all users");
 		return new ArrayList<>(internalUserMap.values());
 	}
 
+	/**
+	 * Add user.
+	 *
+	 * @param user the user
+	 */
 	public void addUser(User user) {
 		if(!internalUserMap.containsKey(user.getUserName())) {
 			internalUserMap.put(user.getUserName(), user);
+			logger.debug("Added user: {}", user.getUserName());
 		}
 	}
-	
+
+	/**
+	 * Gets trip deals.
+	 *
+	 * @param user the user
+	 * @return the trip deals
+	 */
 	public List<Provider> getTripDeals(User user) {
+		logger.debug("Getting trip deals for user: {}", user.getUserName());
 		int cumulatativeRewardPoints = user.getUserRewards().stream().mapToInt(UserReward::getRewardPoints).sum();
-		List<Provider> providers = tripPricer.getPrice(tripPricerApiKey, user.getUserId(), user.getUserPreferences().getNumberOfAdults(), 
+		List<Provider> providers = tripPricer.getPrice(tripPricerApiKey, user.getUserId(), user.getUserPreferences().getNumberOfAdults(),
 				user.getUserPreferences().getNumberOfChildren(), user.getUserPreferences().getTripDuration(), cumulatativeRewardPoints);
 		user.setTripDeals(providers);
 		return providers;
 	}
 
+	/**
+	 * Track user location.
+	 *
+	 * @param user the user
+	 * @return the completable future
+	 */
 	public CompletableFuture<VisitedLocation> trackUserLocation(User user) {
+		logger.debug("Tracking location for user: {}", user.getUserName());
 		return CompletableFuture.supplyAsync(() -> {
 			VisitedLocation visitedLocation = gpsUtil.getUserLocation(user.getUserId());
 			user.addToVisitedLocations(visitedLocation);
@@ -134,17 +217,17 @@ public class TourGuideService {
 	}
 
 	private void addShutDownHook() {
-		Runtime.getRuntime().addShutdownHook(new Thread() { 
-		      public void run() {
-		        tracker.stopTracking();
-		      } 
-		    }); 
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				tracker.stopTracking();
+			}
+		});
 	}
-	
+
 	/**********************************************************************************
-	 * 
+	 *
 	 * Methods Below: For Internal Testing
-	 * 
+	 *
 	 **********************************************************************************/
 	private static final String tripPricerApiKey = "test-server-api-key";
 	// Database connection will be used for external users, but for testing purposes internal users are provided and stored in memory
@@ -156,33 +239,32 @@ public class TourGuideService {
 			String email = userName + "@tourGuide.com";
 			User user = new User(UUID.randomUUID(), userName, phone, email);
 			generateUserLocationHistory(user);
-			
+
 			internalUserMap.put(userName, user);
 		});
 		logger.debug("Created " + InternalTestHelper.getInternalUserNumber() + " internal test users.");
 	}
-	
+
 	private void generateUserLocationHistory(User user) {
 		IntStream.range(0, 3).forEach(i-> {
 			user.addToVisitedLocations(new VisitedLocation(user.getUserId(), new Location(generateRandomLatitude(), generateRandomLongitude()), getRandomTime()));
 		});
 	}
-	
+
 	private double generateRandomLongitude() {
 		double leftLimit = -180;
-	    double rightLimit = 180;
-	    return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
+		double rightLimit = 180;
+		return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
 	}
-	
+
 	private double generateRandomLatitude() {
 		double leftLimit = -85.05112878;
-	    double rightLimit = 85.05112878;
-	    return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
+		double rightLimit = 85.05112878;
+		return leftLimit + new Random().nextDouble() * (rightLimit - leftLimit);
 	}
-	
+
 	private Date getRandomTime() {
 		LocalDateTime localDateTime = LocalDateTime.now().minusDays(new Random().nextInt(30));
-	    return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
+		return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
 	}
-	
 }
